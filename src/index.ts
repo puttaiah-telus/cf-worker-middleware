d9c2fd6e277a71c1df5685fb22b8045114ca9152
puttareddy <puttaiaharugunta@puttaiahs-mbp.home>
2022-12-16 10:51:31 -0500

    updated with private packages

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { handleRequest } from './handler'
 
+// Just to validate telus private packages are retrived/used in worker
+import createLocaleMiddleware from '@telus/next-locale-middleware'
+
+//const middleware = createLocaleMiddleware()
+
 addEventListener('fetch', (event) => {
   event.respondWith(handleRequest(event.request))
 })
